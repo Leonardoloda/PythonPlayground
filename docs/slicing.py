0fdc513c9bc45d2deab76a65438bb3de26d342ed
Leonardo Lopez Daza <leonardoloda2@gmail.com>
2024-07-05 11:35:06 -0500

    docs: slicing

---
+++ docs/slicing.py
@@ -0,0 +1,27 @@
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# You can slice a list in between 2 indexes
+print(list[1:8])
+
+# You can pass an increment
+print(list[0:9:2])
+
+# You can also lift a empty space to gather all elements in that range
+print(list[:2])
+
+# You can also use -1 to step backwards
+print(list[::-1])
+
+tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+# You can slice a list in between 2 indexes
+print(tuple[1:8])
+
+# You can pass an increment
+print(tuple[0:9:2])
+
+# You can also lift a empty space to gather all elements in that range
+print(tuple[:2])
+
+# You can also use -1 to step backwards
+print(tuple[::-1])
