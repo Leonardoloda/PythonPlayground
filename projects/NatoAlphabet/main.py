aa886b89f33cdbe2cc2f5530c97596489ebc3a9c
Leonardo <leonardoloda2@gmail.com>
2024-07-11 16:58:25 -0500

    refactor: nato project now with error handling

--- projects/NatoAlphabet/main.py
+++ projects/NatoAlphabet/main.py
@@ -1,32 +1,43 @@
 import pandas
 
-alphabet = pandas.read_csv("files/nato_phonetic_alphabet.csv")
+try:
+    alphabet = pandas.read_csv("files/nato_phonetic_alphabet.csv")
+except FileNotFoundError:
+    print("ERROR: Missing config file")
+    exit()
 
 user_input = input("Enter a word: ")
 
+'''
 # Version 1
+def convert_letter(letter):
+    """ Gets the nato letter for any letter """
+    try:
+        return alphabet[alphabet.letter == letter.upper()].values[0][1]
+    except IndexError:
+        return letter
 
-convert_letter = lambda letter: alphabet[alphabet.letter == letter.upper()].values[0][1]
 
 translated = [convert_letter(letter) for letter in user_input]
 response = " ".join(translated)
 
 print(f"Here's your word translated: {response}")
+'''
+
+
 
 # Version 2
 
 nato_alphabet = {letter: code for (_, letter, code) in alphabet.itertuples()}
 
-converted = [nato_alphabet[letter.upper()] for letter in user_input]
-converted_response = " ".join(converted)
+def translate_letter(letter):
+    """Gets the NATO letter for the input"""
+    try:
+        return nato_alphabet[letter]
+    except KeyError:
+        return letter
+    
 
-print(f"Here's version 1 result: {converted_response}")
+response = [translate_letter(letter.upper()) for letter in user_input]
 
-# Version 3
-
-nato_alphabet = {row.letter: row.code for (_, row) in alphabet.iterrows()}
-
-converted = [nato_alphabet[letter.upper()] for letter in user_input]
-converted_response = " ".join(converted)
-
-print(f"Here's version 1 result: {converted_response}")
+print(f"Here's version 1 result: {''.join(response)}")
