782b1f748540eb87accb97fca03e621e2a2255b6
Leonardo Lopez Daza <leonardoloda2@gmail.com>
2024-08-05 10:06:31 -0500

    feature(blogs): added support for contact form

--- projects/Blog/main.py
+++ projects/Blog/main.py
@@ -1,5 +1,17 @@
-from flask import Flask, render_template
+from os import getenv
+
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
 from requests import get
+
+from email_client import EmailClient
+
+load_dotenv()
+
+EMAIL = getenv('EMAIL')
+PASSWORD = getenv('PASSWORD')
+
+client = EmailClient(EMAIL, PASSWORD)
 
 app = Flask(__name__)
 
@@ -26,6 +38,28 @@ def post(post_id: int):
 @app.get("/contact")
 def contact_page():
     return render_template("contact.html", title="Contact", bg_image="static/assets/img/contact-bg.jpg")
+
+
+@app.post("/contact")
+def submit_form():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    phone = request.form.get("phone")
+    message = request.form.get("message")
+
+    subject = "A new person has interacted with your blog"
+    body = f"""
+        Name: {name}
+        Email: {email}
+        Phone: {phone}
+        
+        Message: 
+        {message}
+    """
+
+    client.send_email(email=EMAIL, subject=subject, body=body)
+
+    return "Contact form submitted"
 
 
 @app.get("/about")
